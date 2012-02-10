c5d175773bc244967daab1f878f68e72bef7b13e
quezo <alex.quez@gmail.com>
2012-02-10 01:11:51 -0800

    Making key bindings work

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,115 @@
+var KeyBindings = (function() {
+
+	/***********************
+	* Manages the key bindings for Tinkerbox
+	*
+	* Tinkerbox, let's get fancy
+	* Tinkerbox, women wear dresses, men use Tinkerbox
+	* Tinkerbox, don't let your mom catch you using it
+	* Tinkerbox, hide your women, hide your childen
+	************************/
+
+	var KeyBindings = {
+
+		lastKeyPressed:  0,
+		altKeyPressedPreviously: false,
+		
+		HTMLeditor: '', 
+		CSSeditor: '', 
+		JSeditor: '',
+
+	    init: function(HTMLeditor, CSSeditor, JSeditor) {
+	    	this.bindKeys();
+	    	this.HTMLeditor = HTMLeditor;
+	    	this.CSSeditor = CSSeditor;
+	    	this.JSeditor = JSeditor;
+	    },
+        
+        // todo, implment CMD-SHIFT-C - Copy current URL
+        bindKeys: function() {
+            $(window).on('keydown keypress', function(event) {
+                // mac os x uses command key (91) as alt key
+                // every other OS uses actual alt key (18)
+                if(KeyBindings.lastKeyPressed == 18 || KeyBindings.lastKeyPressed == 91) {
+                    this.altKeyPressedPreviously = true;
+                }
+                else {
+                    this.altKeyPressedPreviously = false;
+                }
+                
+                // If the user is holding down the cmd key, then you won't get another key press
+                // event for that. Only update lastKeyPressed only if key isn't the same as previous key
+                if(KeyBindings.lastKeyPressed != event.keyCode) {
+                    KeyBindings.lastKeyPressed = event.keyCode;
+                }
+                
+                stop = false;
+                
+                // Process all the altKey pressed events
+                if(altKeyPressedPreviously) {
+                    if(event.keyCode == 49) {
+                        // cmd + 1
+                        KeyBindings.HTMLeditor.focus();
+                        stop = true;
+                    }
+                    else if(event.keyCode == 50) {
+                        // cmd + 2
+                        KeyBindings.CSSeditor.focus();
+                        stop = true;
+                    }
+                    else if(event.keyCode == 51) {
+                        // cmd + 3
+                        KeyBindings.JSeditor.focus();
+                        stop = true;
+                    }
+                    else if(event.keyCode == 52) {
+                        // cmd + 4
+                        console.log('give focus to result area');
+                        // alextodo, how does this work? how does it get focus?
+                        stop = true;
+                    }
+                    else if(event.keyCode == 67) {
+                        // cmd + c
+                        // compile and run code
+                        console.log('compile');
+                    }
+                    else if(event.keyCode == 70) {
+                        // command + f
+                        // fork this project
+                        console.log('fork');
+                    }
+                    else if(event.keyCode == 71) {
+                        // command + g
+                        // create a gist
+                        console.log('gist');
+                    }
+                    else if(event.keyCode == 78) {
+                        // command + n
+                        // create a new tinker box
+                        console.log('create new tinker box');
+                        stop = true;
+                    }
+                    else if(event.keyCode == 83) {
+                        // command + s
+                        stop = true;
+                    }
+                }
+                
+                if(stop) {
+                    $.Event(event).stopPropagation();
+                    
+                    return false;
+                }
+            });
+        }
+    };
+
+	// This ends the KeyBindings module
+
+	return KeyBindings;
+
+})();
+
 (function($) {
 
 	// "GLOBALS"
@@ -138,5 +250,8 @@
     $('input[name="js-preprocessor"]').on('click', function() {
     	TBDB.setCSSOption('preprocessor', this.value);
     });
+
+    // Bind keys
+    KeyBindings.init(HTMLeditor, CSSeditor, JSeditor);
 
 })(jQuery);
