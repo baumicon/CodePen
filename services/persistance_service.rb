1927df477960ad9ae2c74c2a833ee5c57fb2dd30
Timothy Sabat <timothy.sabat@gmail.com>
2012-02-19 22:57:45 -0800

    Passing persistence tests

--- services/persistance_service.rb
+++ services/persistance_service.rb
@@ -6,18 +6,29 @@ require '../lib/json_util'
 class PersistanceService
 
   def save_content(session_id, json)
-    user = User.get_by_session_id(session_id)
-    slugs = Slug.where(:uid => user.uid).all.map{|slug| slug.name}
-    content = Content.new_from_json(json, user.uid, slugs)
-
-    return {'success' => true} unless not content.valid?
     begin
+      user = User.get_by_session_id(session_id)
+      slugs = Slug.where(:uid => user.uid).all.map{|slug| slug.name}
+      content = Content.new_from_json(json, user.uid, slugs)
+
+      if not slugs.include? content['slug_name']
+        slug = Slug.new(:uid => user.uid, :name => content['slug_name'])
+        return errors(slug.errors) unless slug.valid?
+        slug.save
+        content.slugs << slug.name
+      end
+
+      return errors(content.errors) unless content.valid?
       content.save
-    rescue
+      {'success' => true}
+    rescue Exception => ex
+      throw ex
       #TODO: learn how to get a safe stack trace to display.  For now, generic message
-      return {'success' => false, 'errors' => {:unable_to_save => 'get error stack'}}
+      return errors({:unable_to_save => 'Error Saving From Persistance Service'})
     end
-    {'success' => false, 'errors' => content.errors}
   end
 
+  def errors(errors)
+    {'success' => false, 'errors' => errors}
+  end
 end
--- spec/persistance_service_integration_spec.rb
+++ spec/persistance_service_integration_spec.rb
@@ -3,25 +3,37 @@
 require '../services/persistance_service'
 MongoMapper.database = 'integration_test'
 
-def setup
-  Slug.collection.remove
-  Content.collection.remove
-  User.collection.remove
-
-  require 'awesome_print'
-  ap 'hi'
-  Slug.new(:uid => '7', :name => 'duder').save
-  Slug.new(:uid => '7', :name => 'testing').save
-  ap Slug.all
-  User.new(:uid => '7', :name => 'user', :provider => 'twitter', :nickname => 'booger').save
+def clear_db
+  MongoMapper.database.collections.each do |collection|
+    collection.remove
+  end
 end
 
 describe PersistanceService do
-  it "should save without error", :type => 'integation' do
-    require 'awesome_print'
-    ap 'hello'
-    setup
+
+  it "should save if user already owns slug" do
+    clear_db
+    Slug.new(:uid => '7', :name => 'testing').save
+    User.new(:uid => '7', :name => 'user', :provider => 'twitter', :nickname => 'booger').save
+
     service = PersistanceService.new
     service.save_content('7', '{"slug_name":"testing", "version":"5"}')['success'].should == true
+  end
+
+  it "should save if slug is new and not already taken" do
+    clear_db
+    User.new(:uid => '7', :name => 'user', :provider => 'twitter', :nickname => 'booger').save
+    service = PersistanceService.new
+    result = service.save_content('7', '{"slug_name":"new_slug", "version":"5"}')
+    result['success'].should == true
+  end
+
+  it "should NOT save if slug is already taken by another user" do
+    clear_db
+    User.new(:uid => '7', :name => 'user', :provider => 'twitter', :nickname => 'booger').save
+    Slug.new(:name => 'new_slug', :uid => '5').save
+    service = PersistanceService.new
+    result = service.save_content('7', '{"slug_name":"new_slug", "version":"5"}')
+    result['success'].should == false
   end
 end
