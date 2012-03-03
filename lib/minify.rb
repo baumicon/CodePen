81b5f76b4d4669f7ca2afcb02db95298ef177422
quezo <alex.quez@gmail.com>
2012-03-03 09:05:44 -0800

    Having minify create a versioned directory. Makes for a quicker cutover to new JS files and ensures users don't miss anything while we cutover to newest JS.

--- lib/minify.rb
+++ lib/minify.rb
@@ -20,7 +20,7 @@ VERSION_FILE = 'version.txt'
 
 module PathUtil
   def clean_path(path)
-    path.sub('//', '/')
+    path.gsub('//', '/')
   end
 end
 
@@ -58,10 +58,9 @@ class Minify
   end
   
   def get_src_to_prodjs(scripts, prod_filename)
-    clean_path(PRODJS_DIR + prod_filename + '.' + get_version() + '.js')
+    clean_path(PRODJS_DIR + '/' + get_version() + '/' + prod_filename + '.js')
   end
   
-  # alextodo, on startup, you should load this anytime this is restarted
   def get_version
     path = clean_path(@project_path + PUBLIC_DIR + PRODJS_DIR)
     version = File.read(path + VERSION_FILE)
@@ -77,12 +76,18 @@ class MinifyProject
   @js_path
   @prodjs_path
   
+  @version
+  
+  # a filepath to prod js file name hash
+  @prod_file_names
+  
   include PathUtil
   
   def initialize(tpl_path, js_path, prodjs_path)
     @tpl_path = clean_path(tpl_path)
     @js_path = clean_path(js_path)
     @prodjs_path = clean_path(prodjs_path)
+    @prod_file_names = Hash.new
   end
   
   def minify_all_js_files
@@ -91,42 +96,59 @@ class MinifyProject
     # named after the template they were created for.
     # 
     # For example the template home_page.html with the following filter:
-    #     {{ ['/js/jquery.js', '/js/home.js']|js_script|safe }}
+    #     <%= js_scripts(['/js/jquery.js', '/js/home.js'] %>
     #     
     # Will create a production file named:
-    #     /prodjs/home_page.[version].js
+    #     /prodjs/version/home_page.js
     #     
     # The version number is stored in the /prodjs/version.txt file.
+    load_minify_attributes
+    prep_directory
     
-    prep_prodjs_dir()
-    
-    files = index_project_files()
+    files = index_project_files
     filenames_to_scripts = get_filename_to_scripts(files)
     
     filenames_to_scripts.each do |filename, scripts|
       minify_page_scripts(filename, scripts)
     end
+    
+    write_version_file
+    clean_up_directory
   end
   
   private
   
-  def prep_prodjs_dir
-    # Prepare the /prodjs/ directory for minification
-    # Remove all the files in this directory
-    if File.directory?(@prodjs_path)
-      FileUtils.remove_dir @prodjs_path
-    end
-    
-    # Make sure the prod js dir exists
-    FileUtils.makedirs @prodjs_path
-    
+  # Private: Load version numbers used for the rest of the process
+  #
+  def load_minify_attributes
     # use unix timestamp a version
     @version = Time.now.to_i.to_s
-    
+    @new_js_dir = clean_path(@prodjs_path + '/' + @version)
+  end
+  
+  def prep_directory
+    # Make the new version directory
+    FileUtils.makedirs @new_js_dir
+  end
+  
+  def write_version_file
     # Make the /prodjs/version.txt file and save current unix timestamp
     version_file = File.new(clean_path(@prodjs_path + '/' + VERSION_FILE), 'w')
     version_file.write(@version)
     version_file.close()
+  end
+  
+  # Private: Delete the old directory with old production files
+  def clean_up_directory
+    dirs = Dir.glob(@prodjs_path + '**')
+    
+    dirs.each do |d|
+      save_dir = clean_path(@prodjs_path + '/' + @version)
+      
+      if File.directory?(d) and save_dir != clean_path(d)
+        FileUtils.remove_dir d
+      end
+    end
   end
   
   def index_project_files
@@ -162,7 +184,7 @@ class MinifyProject
   
   def find_js_scripts(path_to_file)
     # Find the js_scripts embedded in our templates
-    #     ex. ['/js/jquery.js', '/js/bootstrap-dropdown.js', '/js/doula.js']
+    #     ex. (['/js/jquery.js', '/js/bootstrap-dropdown.js', '/js/doula.js'], 'prodfilename')
     js_scripts = ''
     contents = get_clean_contents(path_to_file)
     
@@ -171,6 +193,8 @@ class MinifyProject
     contents.scan(/js_scripts\(\[(.*)\],\s?'([\w\d]+)'/) do |match|
       if $1
         js_scripts = $1
+        # store the path to file as key to the future prod file name
+        @prod_file_names[path_to_file] = $2
       end
     end
     
@@ -185,6 +209,8 @@ class MinifyProject
     contents.gsub(/\s+/, ' ')
   end
   
+  # Private: Minify all the scripts found in a single file
+  #   write those files to the production directory
   def minify_page_scripts(filename, scripts)
     scripts = get_cleaned_scripts(scripts)
     minified_js = get_minified_js(scripts)
@@ -226,25 +252,23 @@ class MinifyProject
     minified_js
   end
   
+  # Private: Return the production js file name
+  #   Name of file is the second argument passed to js_scripts
+  #   Checks to see if the filename already has the .js extension
   def get_prod_filename(filename)
-    # Return the production file name:
-    #     Format template_name.version.js
-    rel = filename.sub(@tpl_path, '')
-    prod_file = File.basename(rel, ".erb")
+    prod_filename = @prod_file_names[filename]
     
-    prod_file + '.' + @version + '.js'
+    if !prod_filename.end_with?('.js')
+      prod_filename += '.js'
+    end
+    
+    prod_filename
   end
   
   def create_prod_file(prod_filename, minified_js)
     # Actually write the minified files
-    prod_file_path = clean_path(@prodjs_path + '/' + prod_filename)
-    # Make sure the prod js dir exists
-    prod_dir = File.dirname(prod_file_path)
-    
-    if !File.directory?(prod_dir)
-      FileUtils.makedirs prod_dir
-    end
-    
+    prod_file_path = clean_path(@new_js_dir + '/' + prod_filename)
+        
     prod_file = File.new(prod_file_path, 'w')
     prod_file.write(minified_js)
     prod_file.close()
