1ede99960a976eb0c7f388caebf91e51266f44e1
Timothy Sabat <timothy.sabat@gmail.com>
2012-03-16 14:14:44 -0700

    Adding some extra exception stuff

--- models/incrementor.rb
+++ models/incrementor.rb
@@ -9,10 +9,17 @@ class Incrementor
     begin
       model = MongoMapper.database.collection(:incrementors).find_and_modify(
         :query => {'name' => name}, :update => {'$inc' => {:count => 1}}, :new => true, :safe => true)
-    rescue
-      Incrementor.new(:name => name, :count => 0).save
-      model = MongoMapper.database.collection(:incrementors).find_and_modify(
-        :query => {'name' => name}, :update => {'$inc' => {:count => 1}}, :new => true, :safe => true)
+    rescue Exception ex
+      ap 'in first exception'
+      ap ex
+      begin
+        Incrementor.new(:name => name, :count => 0).save
+        model = MongoMapper.database.collection(:incrementors).find_and_modify(
+          :query => {'name' => name}, :update => {'$inc' => {:count => 1}}, :new => true, :safe => true)
+      rescue Exception ex
+        ap 'in second exception'
+        ap ex
+      end
     end
     model['count']
   end
