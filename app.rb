b9db6ba21568c0bab9345cc74bb30c4a6229439e
Timothy Sabat <timothy.sabat@gmail.com>
2012-03-16 13:52:36 -0700

    Added a debug statement showing content on command line

--- app.rb
+++ app.rb
@@ -109,13 +109,14 @@ class App < Sinatra::Base
     # TODO: this is a hack.  we need to return a non-json version
     # and deal with errors in flash.  Same with below.
     content = JSON.parse(Content.latest(slug))
+    ap content
     @iframe_src = get_iframe_url(request)
     @c_data = encode(content['payload'].to_json) or {}.to_json
     erb :index
   end
 
   # anon user
-  get %r{/(\d)/(\d)} do |slug, version|
+  get %r{/(\d+)/(\d+)} do |slug, version|
     content = JSON.parse(Content.version(slug, version))
     @iframe_src = get_iframe_url(request)
     @c_data = encode(content['payload'].to_json) or {}.to_json
