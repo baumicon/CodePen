d3563fa07d4101331cdbf291828946dd95de3be3
quezo <alex.quez@gmail.com>
2012-03-16 16:16:13 -0700

    Adding version number, loading properly from db

--- lib/sessionator.rb
+++ lib/sessionator.rb
@@ -1,19 +1,21 @@
 require './models/user'
 require './models/incrementor'
+require 'digest'
 
 module Sessionator
 
   use Rack::Session::Cookie, :key => 'codepen'
 
   def set_session
-
     if uid = session['uid']
       @user = User.first(:uid => uid, :sort => :uid.desc)
       if @user.nil?
+        puts 'gonna clear the session here'
         session.clear
         set_session
       end
     else
+      puts 'using this session'
       @user = User.new(:uid => Incrementor.next_count('user'))
       @user.save
     end
@@ -21,4 +23,12 @@ module Sessionator
     session['uid'] = @user.uid
   end
 
+  def set_auth_token
+    salt = 'css-tricks'
+    session['auth_token'] = Digest::SHA1.hexdigest(salt + Time.now.to_i.to_s)
+  end
+
+  def valid_auth_token?(auth_token)
+    auth_token == session['auth_token']
+  end
 end
