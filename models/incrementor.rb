fad78790cb0b1b793977f2182e33520c02a80242
Timothy Sabat <timothy.sabat@gmail.com>
2012-03-16 15:10:47 -0700

    fixing mongo incrementor

--- models/incrementor.rb
+++ models/incrementor.rb
@@ -9,18 +9,15 @@ class Incrementor
     begin
       model = MongoMapper.database.collection(:incrementors).find_and_modify(
         :query => {'name' => name}, :update => {'$inc' => {:count => 1}}, :new => true, :safe => true)
-    rescue Exception => ex
-      ap 'in first exception'
-      ap ex
-      begin
+      if not model
         Incrementor.new(:name => name, :count => 0).save
         model = MongoMapper.database.collection(:incrementors).find_and_modify(
           :query => {'name' => name}, :update => {'$inc' => {:count => 1}}, :new => true, :safe => true)
-      rescue Exception => ex
-        ap 'in second exception'
-        ap ex
       end
+    rescue Exception => ex
+      puts "exception in incrementor!"
     end
+
     model['count']
   end
 
