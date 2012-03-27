010f8f5485d3345096b4f6374e4fd92d00531f80
Chris Coyier <chriscoyier@gmail.com>
2012-03-26 20:05:47 -0700

    very start to keyboard popup

--- public/js/key_bindings.js
+++ public/js/key_bindings.js
@@ -12,62 +12,61 @@ var KeyBindings = {
             // Process all the altKey pressed events
             if(event.altKey) {
                 if(event.keyCode == 49) {
-                    // cmd + 1
+                    // 1
                     stop = true;
                     Main.openExpandedArea('#box-html');
                     HTMLEditor.setCursorToEnd();
                     Main.refreshEditors(200);
                 }
                 else if(event.keyCode == 50) {
-                    // cmd + 2
+                    // 2
                     stop = true;
                     Main.openExpandedArea('#box-css');
                     CSSEditor.setCursorToEnd();
                     Main.refreshEditors(200);
                 }
                 else if(event.keyCode == 51) {
-                    // cmd + 3
+                    // 3
                     stop = true;
                     Main.openExpandedArea('#box-js');
                     JSEditor.setCursorToEnd();
                     Main.refreshEditors(200);
                 }
-                else if(event.keyCode == 13) {
-                    // cmd + return
-                    // compile and run code
-                    stop = true;
-                    CodeRenderer.compileContent(true);
-                }
+                //
+                // Not using this one anymore
+                //
+                // else if(event.keyCode == 13) {
+                //     // return
+                //     // compile and run code
+                //     stop = true;
+                //     CodeRenderer.compileContent(true);
+                // }
+                //
                 else if(event.keyCode == 75) {
                     // command + K
                     // fork this project
-                    // alextodo, what does fork this project mean?
-                    // start with another, save to local storage?
-                    // then start using that? yea
+                    // Does this work?
                     Data.forkData();
                     window.open('/');
                 }
                 else if(event.keyCode == 71) {
-                    // command + g
+                    // g
                     // create a gist
                     stop = true;
                     CodeRenderer.createGist();
                 }
                 else if(event.keyCode == 83) {
-                    // command + s
+                    // s
                     Data.save();
-                    
                     stop = true;
                 }
                 else if(event.keyCode == 76) {
-                    // command + l
-                    
+                    // l
                     // If you've saved your code, you can open it in full page
                     // mode. Otherwise this fails silently.
                     if(document.location.pathname.match(/\/[\d]+(\/[\d]+)?/)) {
                         window.open(document.location.pathname + '/full')
                     }
-                    
                 }
             }
             
